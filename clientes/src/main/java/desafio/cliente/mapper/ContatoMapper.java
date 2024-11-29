40154390c2c1fe3d79d933c68dde0a627ed7bb04
João Salvador Neto <joao72neto@gmail.com>
2024-11-29 17:23:36 -0300

    Use notações para criar mappers

--- clientes/src/main/java/desafio/cliente/mapper/ContatoMapper.java
+++ clientes/src/main/java/desafio/cliente/mapper/ContatoMapper.java
@@ -1,24 +1,15 @@
 package desafio.cliente.mapper;
 
+import org.mapstruct.Mapper;
+
 import desafio.cliente.dto.ContatoDTO;
 import desafio.cliente.entity.Contato;
 
-public class ContatoMapper {
-    public static ContatoDTO toDTO(Contato contato){
-        ContatoDTO dto = new ContatoDTO(contato.getId(), 
-                                        contato.getTipo(),
-                                        contato.getTexto());
+@Mapper
+public interface ContatoMapper {
+ 
+    ContatoDTO toDTO(Contato contato);
 
-        return dto;
-    }
+    Contato toEntity(ContatoDTO contatoDTO);
 
-    public static Contato toEntity(ContatoDTO dto){
-        Contato contato = new Contato();
-
-        contato.setId(dto.id());
-        contato.setTexto(dto.texto());
-        contato.setTipo(dto.tipo());
-
-        return contato;
-    }
 }
